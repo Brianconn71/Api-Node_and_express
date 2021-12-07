fd8f00fbaa9167bf3954caa11ea0924761388c10
Brian Connolly <Brian.connolly71@gmail.com>
2021-12-07 21:33:22 +0000

    scraped the guardioan for climate change news

--- index.js
+++ index.js
@@ -1,11 +1,39 @@
 const PORT = 8000
-
 const express = require('express')
-
 const axios = require('axios')
-
 const cheerio = require('cheerio')
 
 const app = express()
+
+const newspapers = [
+    {
+        name: 'thetimes',
+    }
+]
+
+const articles = []
+
+app.get('/', (req, res) => {
+    res.json('Welcome to my Climate Change News API')
+})
+
+app.get('/news', (req,res) => {
+    axios.get('https://www.theguardian.com/environment/climate-crisis', )
+        .then((response) => {
+            const html = response.data
+            const $ = cheerio.load(html)
+
+            // looks for any a tag on page containing climate
+            $('a:contains("climate")', html).each(function () {
+                const title = $(this).text()
+                const url = $(this).attr('href')
+                articles.push({
+                    title,
+                    url
+                })
+            })
+            res.json(articles)
+        }).catch((err) => console.log(err))
+})
 
 app.listen(PORT, () => console.log(`server running on port ${PORT}`))
