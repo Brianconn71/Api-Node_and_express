4bf49b9a7c0fc5489d5ccd1c16dbfcde29b26392
Brian Connolly <Brian.connolly71@gmail.com>
2021-12-08 21:06:48 +0000

    add: added numerous newspapers to the api

--- index.js
+++ index.js
@@ -8,32 +8,56 @@ const app = express()
 const newspapers = [
     {
         name: 'thetimes',
-    }
+        address: 'https://www.thetimes.co.uk/environment/climate-change',
+        base: ''
+    },
+    {
+        name: 'theguardian',
+        address: 'https://www.theguardian.com/environment/climate-change',
+        base: ''
+    },
+    {
+        name: 'telegraph',
+        address: 'https://www.telegraph.co.uk/climate-change',
+        base: 'https://www.telegraph.co.uk'
+    },
 ]
 
 const articles = []
+
+newspapers.forEach(newspaper => {
+    axios.get(newspaper.address)
+        .then(response => {
+            const html = response.data
+            const $ = cheerio.load(html)
+
+            $('a:contains("climate")', html).each(function () {
+                const title = $(this).text()
+                const url = $(this).attr('href')
+
+                articles.push({
+                    title,
+                    url: newspaper.base + url,
+                    source: newspaper.name
+                })
+            })
+        })
+})
 
 app.get('/', (req, res) => {
     res.json('Welcome to my Climate Change News API')
 })
 
 app.get('/news', (req,res) => {
-    axios.get('https://www.theguardian.com/environment/climate-crisis', )
-        .then((response) => {
-            const html = response.data
-            const $ = cheerio.load(html)
+    res.json(articles)
+})
 
-            // looks for any a tag on page containing climate
-            $('a:contains("climate")', html).each(function () {
-                const title = $(this).text()
-                const url = $(this).attr('href')
-                articles.push({
-                    title,
-                    url
-                })
-            })
-            res.json(articles)
-        }).catch((err) => console.log(err))
+app.get('/news/:newspaperId', async (req,res) => {
+    const newspaperId = req.params.newspaperId
+
+    newspapers.filter(newspaper => )
+
+    axios.get()
 })
 
 app.listen(PORT, () => console.log(`server running on port ${PORT}`))
